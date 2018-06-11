6e1465c3775d08e2b47f79fc7b29a3bfad879b34
igsong <silverfox516@hotmail.com>
2018-06-11 18:31:37 +0900

    navbar

--- __init__.py
+++ __init__.py
@@ -7,5 +7,13 @@ app = Flask(__name__)
 def homepage():
 	return render_template("main.html")
 
+@app.route('/page1')
+def page1():
+	return render_template("page1.html")
+
+@app.route('/nav')
+def nav():
+	return render_template("nav.html")
+
 if __name__ == "__main__":
 	app.run(debug=True)
